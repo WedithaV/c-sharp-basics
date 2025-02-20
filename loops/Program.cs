49faf075431d43340162b846f0c7eafac601e920
WedithaV <wedithav@gmail.com>
2025-02-20 22:27:08 +0530

    foreach loop

---
+++ loops/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] names = new string[3];
+names[0] = "Weditha";
+names[1] = "Vishwa";
+names[2] = "Prabhath";
+
+foreach (string name in names){
+    Console.WriteLine(name);
+}
+
+int[] numbers = {1,2,3,4,5,6};
+
+foreach(int num in numbers){
+    Console.WriteLine(num);
+}
