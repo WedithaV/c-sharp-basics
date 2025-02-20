b7f608ef817f85c98dde3ef8e61441443cba2cd6
WedithaV <wedithav@gmail.com>
2025-02-20 23:23:19 +0530

    Switch case

---
+++ switch/Program.cs
@@ -0,0 +1,17 @@
+﻿display(4,4);
+display(3,5);
+display(-3,5);
+
+void display(int a, int b){
+    switch(a,b){
+        case var(x,y) when x > 0 && y > 0 && x == y:
+        Console.WriteLine("Values are valid and equal");
+        break;
+        case var(x,y) when x > 0 && y > 0:
+        Console.WriteLine("Values are valid");
+        break;
+        default:
+        Console.WriteLine("Invalid values");
+        break;
+    }
+}
