87710f7c543e2938b19b404bb5adb2f07186df01
WedithaV <wedithav@gmail.com>
2025-02-21 00:00:35 +0530

    Type pattern

---
+++ pattern_matching_switch/Program.cs
@@ -0,0 +1,38 @@
+﻿display('r');
+display('g');
+display('b');
+display('K');
+
+//Value pattern
+void display(char color){
+    var result = color switch{
+        'r' => "color is red",
+        'g' => "color is green",
+        'b' => "color is blue",
+        _ => "Unknown color"
+    };
+    Console.WriteLine(result);
+}
+
+
+//Type pattern
+
+int num = 10;
+string name = "Weditha";
+List<string> cities = new List<string> {"Colombo","Kandy","Galle"};
+var numbers = new  int[] {1,2,3,4,5};
+double salary = 10000.00;
+
+object CheckType(object val) => val switch{
+    int => "Integer",
+    string => "String",
+    List<string> => "List of strings",
+    Array => "Array",
+    _ => "Unknown type"
+};
+
+CheckType(num);
+CheckType(name);
+CheckType(cities);
+CheckType(numbers);
+CheckType(salary);
