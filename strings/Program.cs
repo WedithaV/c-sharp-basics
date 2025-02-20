dd7f1312b33ca175a3987d14ece75616a4833560
WedithaV <wedithav@gmail.com>
2025-02-21 00:21:03 +0530

    strings

---
+++ strings/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Hello");
+Console.Write("World");
+
+Console.Write("\n");
+
+Console.WriteLine("Hello\nWorld");
+
+Console.WriteLine("Weditha\tVishwa\tPrabhath");
+
+Console.WriteLine("The word \"Hello\" ");
+
+Console.WriteLine(@"The word \");
+Console.WriteLine(@"C:\Users\wedit\Desktop\c#\c-sharp-basics\strings");
+
+//String concat
+
+string firstName = "Weditha";
+string greeting = "Hello, ";
+
+Console.WriteLine(firstName+greeting+" Good evening !");
+
+Console.WriteLine("File Path is "+ @"C:\Users\wedit\Desktop\c#\c-sharp-basics\strings");
+
+
+
+//string interpolation
+Console.WriteLine($"Hello, {firstName}");
+Console.WriteLine($"Hello, {firstName} - Today is {DateTime.Now}");
