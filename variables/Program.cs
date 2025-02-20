6b46b2cc9d1a8973ca1de2c458e2f82bf0a4dd0d
WedithaV <wedithav@gmail.com>
2025-02-20 20:03:30 +0530

    variables

---
+++ variables/Program.cs
@@ -0,0 +1,18 @@
+﻿string name = "Weditha";
+int age = 24;
+decimal height = 179.23M;
+char dtx = 'H';
+bool value = true;
+
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(height);
+Console.WriteLine(dtx);
+Console.WriteLine(value);
+
+
+var trex = 3;
+
+//When using var . the value should assign when declaring it . otherwise it will give an error
+//And also its better to use trex . otherwise it will give and warning
+Console.WriteLine(trex);
