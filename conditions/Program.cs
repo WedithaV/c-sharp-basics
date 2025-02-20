87330289445904248fd50c39016d7973cc160aa1
WedithaV <wedithav@gmail.com>
2025-02-20 22:25:41 +0530

    Conditions

---
+++ conditions/Program.cs
@@ -0,0 +1,19 @@
+﻿var dice = new Random();
+var roll1 = dice.Next(1,7);
+var roll2 = dice.Next(1,7);
+var roll3 = dice.Next(1,7);
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine(roll1+" "+roll2+" "+roll3);
+
+if((roll1 == roll2) || (roll1 == roll3) || (roll2 == roll3)){
+    total += 2;
+}
+if(total > 6){
+    Console.WriteLine("You win");
+}
+else{
+    Console.WriteLine("You Loose");
+}
+
+Console.WriteLine(total);
